f918ce33d7c396d96203f91ff15fbeab839d753f
Jason Pepas <pepaslabs@gmail.com>
2015-03-05 23:19:34 -0600

    removing compatibility for older arduino environments

--- WriteOnlySoftSPI.h
+++ WriteOnlySoftSPI.h
@@ -36,11 +36,7 @@
 #ifndef _WRITEONLYSOFTSPI_H
 #define _WRITEONLYSOFTSPI_H
 
-#if (ARDUINO >= 100) 
-# include <Arduino.h>
-#else
-# include <WProgram.h>
-#endif
+#include <Arduino.h>
 
 #include <SPI.h>
 
