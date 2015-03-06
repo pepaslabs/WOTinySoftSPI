8bc83d84a486af4de4234b213de7a1796a787e03
Jason Pepas <pepaslabs@gmail.com>
2015-03-05 23:42:04 -0600

    pulling in SPI_MODE defines

--- WOTinySoftSPI.h
+++ WOTinySoftSPI.h
@@ -38,8 +38,10 @@
 
 #include <Arduino.h>
 
-//#include <SPI.h>
-// ATTiny's can't include SPI.h, so the following constants have been copied in:
+// These constants have been copied from SPI.h because
+// ATTiny chips can't import SPI.h.
+#ifndef SPI_CLOCK_DIV4
+
 #define SPI_CLOCK_DIV4 0x00
 #define SPI_CLOCK_DIV16 0x01
 #define SPI_CLOCK_DIV64 0x02
@@ -47,6 +49,17 @@
 #define SPI_CLOCK_DIV2 0x04
 #define SPI_CLOCK_DIV8 0x05
 #define SPI_CLOCK_DIV32 0x06
+//#define SPI_CLOCK_DIV64 0x07
+
+#define SPI_MODE0 0x00
+#define SPI_MODE1 0x04
+#define SPI_MODE2 0x08
+#define SPI_MODE3 0x0C
+#define SPI_MODE_MASK 0x0C  // CPOL = bit 3, CPHA = bit 2 on SPCR
+#define SPI_CLOCK_MASK 0x03  // SPR1 = bit 1, SPR0 = bit 0 on SPCR
+#define SPI_2XCLOCK_MASK 0x01  // SPI2X = bit 0 on SPSR
+
+#endif
 
 
 class WOTinySoftSPI {
